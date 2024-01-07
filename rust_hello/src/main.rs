7fda7161ca5a29d4cf59ad98d415d60b30081573
Naveen Kumar Gutti <naveenkumar.gvvsnr@gmail.com>
2024-01-07 17:11:50 +0530

    Rust hello world and cross compilation

---
+++ rust_hello/src/main.rs
@@ -0,0 +1,6 @@
+use current_platform::{COMPILED_ON, CURRENT_PLATFORM};
+fn main() {
+    println!("Hello, SammyTechPlayground friends!");
+    println!("Compiled on {}", COMPILED_ON);
+    println!("Running on {}", CURRENT_PLATFORM);
+}
